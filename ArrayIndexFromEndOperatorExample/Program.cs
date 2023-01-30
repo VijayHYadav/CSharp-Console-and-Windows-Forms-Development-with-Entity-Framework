90db3f788067a09d27b62518d57e8a715df64fbd
VijayHYadav <vizzyadav1997@gmail.com>
2023-01-30 19:47:16 +0530

    160. Index-From-End Operator

---
+++ ArrayIndexFromEndOperatorExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayIndexFromEndOperatorExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[4] { 3, 1, 2, 4 };
+
+            // index-from-end operator
+            int result = a[^1];
+            Console.WriteLine(result); // Output: 60
+
+            // range operator
+            int[] result2 = a[2..5];
+            Console.WriteLine(result2); // Output: [20,30,40]
+
+            foreach (int i in result2) Console.WriteLine(i);
+        }
+    }
+}
