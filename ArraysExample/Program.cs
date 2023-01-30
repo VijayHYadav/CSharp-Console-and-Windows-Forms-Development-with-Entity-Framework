3b33bf4103156e6fb2258d00ad6dc85e9f4d5bf9
VijayHYadav <vizzyadav1997@gmail.com>
2023-01-30 14:57:44 +0530

    150. Creating Arrays

---
+++ ArraysExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ArraysExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create an array, Arrays are stored in the heap and the reference variables are stored in the stack.
+            int[] a = new int[5];
+            int[] b = new int[5] { 1,2,3,4,5};
+            string[] c = new string[5] { "one","two","three","four","five"};
+
+            System.Console.WriteLine(a[0]);
+
+            a[0] = 1;
+            a[1] = 2;
+            a[2] = 3;
+            a[3] = 4;
+            a[4] = 5;
+
+            System.Console.WriteLine(a[0]);
+            System.Console.WriteLine();
+            System.Console.WriteLine(c[0]);
+            System.Console.WriteLine(c[1]);
+            System.Console.WriteLine(c[2]);
+            System.Console.WriteLine(c[3]);
+            System.Console.WriteLine(c[4]);
+
+            System.Console.ReadKey();
+        }
+    }
+}
