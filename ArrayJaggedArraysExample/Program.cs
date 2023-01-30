194ecba6c3076f4cc53f50f10f386fcf13229961
VijayHYadav <vizzyadav1997@gmail.com>
2023-01-30 20:54:04 +0530

    162. Jagged Arrays

---
+++ ArrayJaggedArraysExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayJaggedArraysExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] a = new int[3][];
+            a[0] = new int[3] { 10, 20, 30 };
+            a[1] = new int[3] { 40, 50, 60 };
+            a[2] = new int[3] { 70, 80, 90 };
+
+            // read
+            for(int i=0; i<3; i++)
+            {
+                for (int j = 0; j < a[i].Length; j++)
+                {
+                    Console.WriteLine(a[i][j]);
+                    Console.WriteLine("");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
