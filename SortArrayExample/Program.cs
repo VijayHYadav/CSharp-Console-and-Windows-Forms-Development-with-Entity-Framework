b78367acd7c5f027cf4cf499a16c698498328d0d
VijayHYadav <vizzyadav1997@gmail.com>
2023-01-30 17:50:59 +0530

    159. System.Array.Reverse

---
+++ SortArrayExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SortArrayExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[4] { 3, 1, 2, 4 };
+
+            Array.Sort(a);
+
+            foreach (var item in a)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
