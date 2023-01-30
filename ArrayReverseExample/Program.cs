9634d19ed0fda30728d9073234d1a47a7c2d89f4
VijayHYadav <vizzyadav1997@gmail.com>
2023-01-30 17:53:00 +0530

    159. System.Array.Reverse

---
+++ ArrayReverseExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayReverseExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[4] { 3, 1, 2, 4 };
+
+            // Array.Sort
+            Array.Sort(a);
+
+            foreach (var item in a)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            // Array.Reverse
+            Array.Reverse(a);
+
+            foreach (var item in a)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
