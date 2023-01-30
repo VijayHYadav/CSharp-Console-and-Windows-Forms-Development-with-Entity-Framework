778d5512ecd762e94956fd080e9498f4219281c0
VijayHYadav <vizzyadav1997@gmail.com>
2023-01-30 17:46:14 +0530

    157. System.Array.Resize

---
+++ ResizeExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ResizeExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 1, 2,3, 4 };
+
+            // resize array to 5
+            Array.Resize(ref a, 2);
+
+            foreach (var item in a)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
