dc964d9be711ca5e1e0aff06c106b6706d17c1a6
VijayHYadav <vizzyadav1997@gmail.com>
2023-01-30 14:24:37 +0530

    Creation of Class library

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,5 @@
+﻿public class Customer
+{
+    public int cutomerId;
+    public string name;
+}
--- HelloWorld.NET5/Program.cs
+++ HelloWorld.NET5/Program.cs
@@ -6,7 +6,13 @@ namespace HelloWorld.NET5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Customer customer = new Customer();
+            customer.cutomerId= 1;
+            customer.name= "vijay";
+
+            System.Console.WriteLine(customer.cutomerId);
+            System.Console.WriteLine(customer.name);
+            System.Console.ReadKey();
         }
     }
 }
