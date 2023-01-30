ac0340729da64b1ae62c1097c3a1ae28a8674e7a
VijayHYadav <vizzyadav1997@gmail.com>
2023-01-30 17:40:23 +0530

    156. System.Array.Clear

---
+++ ClearExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * array: this parameter represents the array, in which you want to clear the elements
+ * index: this parameter represents the index, from which clearing process is to be started.
+ * length: this parameter represents the no. of elements that are to be cleared.
+ * **/
+
+namespace ClearExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // create array
+            int[] a = new int[] { 1, 2,3, 4 };
+
+            //clear elements of array, Array.Clear(array, startingIndex, endingIndex);
+            Array.Clear(a, 0, a.Length);
+
+            // display array
+            foreach (int item in a)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
