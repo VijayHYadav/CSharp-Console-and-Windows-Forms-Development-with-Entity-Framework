5514681e82b7d40a8724c8993a0d5037f62462eb
VijayHYadav <vizzyadav1997@gmail.com>
2023-01-31 22:03:41 +0530

    167. List

---
+++ ListExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // create reference variable for List class
+            List<int> myList;
+
+            //create object of List class. 10 is the capacity for the list
+            myList = new List<int>(10) { 1, 2, 3 };
+
+            foreach (int item in myList)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            for (int i = 0; i < myList.Count; i++)
+            {
+                System.Console.WriteLine(myList[i]);
+            }
+
+            System.Console.ReadKey();
+        }
+    }
+}
