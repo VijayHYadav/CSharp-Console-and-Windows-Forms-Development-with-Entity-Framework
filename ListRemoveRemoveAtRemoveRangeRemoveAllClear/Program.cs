0a06dd881aa764cdf0ebc6d27c4094ab35194c10
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-01 07:16:07 +0530

    170. Remove, RemoveAt, RemoveRange, RemoveAll, Clear

---
+++ ListRemoveRemoveAtRemoveRangeRemoveAllClear/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace ListRemoveRemoveAtRemoveRangeRemoveAllClear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> myList = new List<int>() { 10, 20, 30, 40, 50, 60 };
+            
+            myList.Remove(60);
+            myList.RemoveAt(3);
+            myList.RemoveRange(2,2);
+            myList.RemoveAll(n => n == 10);
+            myList.Clear();
+
+            foreach (var item in myList)
+            {
+                System.Console.WriteLine(item);
+            }
+        }
+    }
+}
