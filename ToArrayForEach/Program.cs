cae1588336aad199d461a97a85e7e9ab57500e20
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-01 08:23:16 +0530

    173. ToArray, ForEach

---
+++ ToArrayForEach/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToArrayForEach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //collection
+            List<int> list = new List<int>() { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 40 };
+
+            // conver to array
+            int[] listToArray = list.ToArray();
+
+            foreach (var item in listToArray)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+
+            list.ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
