60daa08b42f88d4a5c6e0116bf8d77d7e6fdffb7
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-04 07:22:01 +0530

    182. Queue

---
+++ QueueExample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<String> queue = new Queue<String>();
+
+            queue.Enqueue("a");
+            queue.Enqueue("b"); 
+            queue.Enqueue("c");
+            queue.Enqueue("d");
+
+            foreach (var item in queue)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            queue.Dequeue();
+            queue.Dequeue();
+
+            System.Console.WriteLine();
+
+            foreach (var item in queue)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            System.Console.WriteLine();
+
+            System.Console.WriteLine(queue.Peek());
+        }
+    }
+}
