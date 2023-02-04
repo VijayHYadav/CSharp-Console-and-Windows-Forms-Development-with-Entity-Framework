af1fea7af8ec408c2804edb99fc6ed71fae7ff69
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-05 05:05:12 +0530

    186. IEnumerable and IEnumerator

---
+++ IEnumerableExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IEnumerableExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // List<string> message = new List<string>() { "hello", "hey", "hi", "hie" };
+            IEnumerable<string> messages = new List<string>() { "hello", "hey", "hi", "hie" };
+
+            foreach (var item in messages)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\n IEnumerator:");
+            IEnumerator<string> enumerator = messages.GetEnumerator();
+            enumerator.Reset();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current);
+            }
+
+        }
+    }
+}
