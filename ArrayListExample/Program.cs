399d7eba0ed4814ff1eee7092e7f9e80286e7906
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-04 06:40:53 +0530

    180. ArrayList

---
+++ ArrayListExample/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListExample
+{
+    class Sample
+    {
+        public int GetNumber()
+        {
+            return 10;
+        }
+
+        public double GetAnotherNumber()
+        {
+            return 10.7;
+        }
+
+        public string GetMessage() {
+
+            return "Hello";
+        }
+
+        public Employee GetEmp()
+        {
+            Employee emp = new Employee();
+            emp.EmpName = "Rocky";
+            return emp;
+        }
+    }
+
+    class Employee
+    {
+        public string EmpName { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sample sm= new Sample();
+            ArrayList list = new ArrayList() { sm.GetNumber(), sm.GetAnotherNumber(), sm.GetEmp(), sm.GetMessage() };
+
+            foreach (var i in list)
+            {
+                if (i is Employee emp)
+                {
+                    Console.WriteLine(emp.EmpName);
+                } else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
