a1d098644f0d88ee0e0e6bd514e1d47058a24aa6
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-10 08:10:37 +0530

    195. IComparer - Part 2

--- IComparerExample/Program.cs
+++ IComparerExample/Program.cs
@@ -13,15 +13,60 @@ namespace IComparerExample
         public string Job { get; set; }
     }
 
+    public enum SortBy
+    {
+        EmpID, EmpName, Job
+    }
+
     class CustomComparer : IComparer<Employee>
     {
-        public int Compare(Employee x, Employee y)
+        /* public int Compare(Employee x, Employee y)
         {
             // String
             // return x.Name.CompareTo(y.Name);
             // Int
              return x.Id - y.Id;
+        }*/
+
+        /* public int Compare(Employee x, Employee y)
+        {
+            int result = 0;
+            if(x.Job != null)
+            {
+                result = x.Job.CompareTo(y.Job);
+            }
+
+            if (result == 0)
+            {
+                if (x.Name != null)
+                {
+                    result = x.Name.CompareTo(y.Name);
+                }
+            }
+
+            return result;
+        }*/
+
+        //Sort by selected column
+        public int Compare(Employee x, Employee y)
+        {
+            int result = 0;
+            switch (this.sortBy)
+            {
+                case SortBy.EmpID:
+                    result = x.Id - y.Id; break;
+                case SortBy.EmpName:
+                    result = (x.Name != null) ? x.Name.CompareTo(y.Name) : 0; break;
+                case SortBy.Job:
+                    result = (x.Job != null) ? x.Job.CompareTo(y.Job) : 0; break;
+                default:
+                    result = 0; break;
+            }
+            return result;
         }
+
+        public SortBy sortBy { get; set; }
+
     }
 
     internal class Program
@@ -47,12 +92,26 @@ namespace IComparerExample
                     Id = 3,
                     Name= "Cest",
                     Job= "Dev"
+                },
+                new Employee()
+                {
+                    Id = 4,
+                    Name= "Best",
+                    Job= "Dev"
+                },
+                new Employee()
+                {
+                    Id = 5,
+                    Name= "Gest",
+                    Job= null
                 }
             };
 
             CustomComparer customComparer = new CustomComparer();
-            
+            customComparer.sortBy = SortBy.EmpName;
+
             employees.Sort(customComparer);
+            //employees.Reverse();
 
             foreach (Employee employee in employees)
             {
