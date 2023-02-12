cfa65c9715a4096a9ef03c68c6cca46dded0ca9b
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-13 02:37:40 +0530

    199. Nested Anonymous Types

---
+++ NestedAnonymousTypes/Program.cs
@@ -0,0 +1,23 @@
+﻿using ClassLibrary7;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedAnonymousTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            var person = new {PersonName = p.GetPersonName(), Age = p.GetPersonAge(), Address = new { Street="abc", City="xyz" } };
+
+            Console.WriteLine(person.PersonName);
+            Console.WriteLine(person.Age);
+            Console.WriteLine(person.Address.Street);
+            Console.WriteLine(person.Address.City);
+        }
+    }
+}
