de72052e4a64641efb655c65b1d1d8da0ceca195
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-13 03:24:36 +0530

    203. Deconstructing

--- ValueTupleExample/Program.cs
+++ ValueTupleExample/Program.cs
@@ -15,6 +15,15 @@ namespace ValueTupleExample
             Customer customer= new Customer();
 
             // get details
+            (int customerId, string customerName, string email) cust = customer.GetCustomerDetails();
+
+            Console.WriteLine(cust.customerId);
+            Console.WriteLine(cust.customerName);
+            Console.WriteLine(cust.email);
+
+            Console.WriteLine("===");
+
+            // get details
             (int customerId, string customerName, string email) = customer.GetCustomerDetails();
 
             Console.WriteLine(customerId);
