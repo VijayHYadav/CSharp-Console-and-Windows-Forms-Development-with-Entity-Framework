2bdfbc85f2b9d68532e40469331a900736468596
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-13 02:24:01 +0530

    198. Anonymous Types

---
+++ AnonymousObjectsExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using ClassLibrary7;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousObjectsExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person p = new Person();
+
+            p.GetPersonName();
+            p.GetPersonAge();
+
+            var person = new { PersonName = p.GetPersonName(), PersonAge = p.GetPersonAge() };
+
+            Console.WriteLine(person.PersonAge);
+            Console.WriteLine(person.PersonName);
+        }
+    }
+}
