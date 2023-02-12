359ef3a9734ae2bbdf9d150795b242eb9730b68a
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-13 03:20:43 +0530

    202. Value Tuples

---
+++ ClassLibrary8/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary8
+{
+    public class Customer
+    {
+        public (int customerId, string customerName, string email) GetCustomerDetails()
+        {
+            return (786, "Scott", "Scott@gmail.com");
+        }
+    }
+}
---
+++ ValueTupleExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using ClassLibrary8;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueTupleExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // create object
+            Customer customer= new Customer();
+
+            // get details
+            (int customerId, string customerName, string email) = customer.GetCustomerDetails();
+
+            Console.WriteLine(customerId);
+            Console.WriteLine(customerName);
+            Console.WriteLine(email);
+        }
+    }
+}
