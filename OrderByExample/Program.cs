5fd0dcde1363a38af0a5b0dd88d7d41140f9e501
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-16 03:51:43 +0530

    215. OrderBy

---
+++ OrderByExample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderByExample
+{
+    class Employee
+    {
+        public int EmpID { get; set; }
+        public string Name { get; set; }
+        public string Job { get; set; }
+        public string City { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //collection of objects
+            List<Employee> employees = new List<Employee>()
+            {
+                new Employee() { EmpID = 1, Name="Henry", Job="Dev", City="Boston"},
+                new Employee() { EmpID = 2, Name="Jenry", Job="DevOps", City="New Boston"},
+                new Employee() { EmpID = 3, Name="Cenry", Job="DevSec", City="Sanfran cisco"},
+                new Employee() { EmpID = 4, Name="Tenry", Job="OpsSec", City="Old Boston"},
+                new Employee() { EmpID = 5, Name="Genry", Job="Developer", City="Tokyo"},
+                new Employee() { EmpID = 6, Name="Renry", Job="OpsSec", City="NY"},
+            };
+
+            // var emps = employees.OrderByDescending(emp => emp.Name).ThenByDescending(emp => emp.EmpID);
+            var emps = employees.OrderBy(emp => emp.Name).ThenBy(emp => emp.EmpID);
+
+            foreach (var item in emps)
+            {
+                Console.WriteLine(item.EmpID + " : " + item.Name + " : " + item.Job + "  : " + item.City);
+            }
+        }
+    }
+}
