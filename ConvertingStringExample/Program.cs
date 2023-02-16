08ee027235d83a83cadb0cfb79e5525dae0a6e65
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-16 14:29:22 +0530

    224. Converting Strings - Part 1

---
+++ ConvertingStringExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertingStringExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Developer";
+            Console.WriteLine(str); // Output:
+            
+            Console.WriteLine(str.ToUpper()); // Output:
+            Console.WriteLine(str.ToLower()); // Output:
+            Console.WriteLine(str);
+
+            Console.WriteLine(str.Substring(2));
+            Console.WriteLine(str.Substring(2, 4));
+
+            Console.ReadKey();
+        }
+    }
+}
