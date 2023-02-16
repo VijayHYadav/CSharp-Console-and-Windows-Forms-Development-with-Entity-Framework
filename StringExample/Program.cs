12b1e33d70f8e3af9e1766740dfd88d1c3d08ba8
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-16 12:15:13 +0530

    222. String

---
+++ StringExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.String x = "hello world";
+            int n = x.Length;
+            char c = x[0];
+            Console.WriteLine(c);
+            Console.WriteLine(x);
+            Console.WriteLine(n);
+            Console.ReadKey();
+        }
+    }
+}
