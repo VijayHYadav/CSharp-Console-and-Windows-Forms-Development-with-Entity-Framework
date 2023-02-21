51ca7e2eb2809a7743870c92c4d1e8e45b537b8f
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-21 07:47:34 +0530

    229. Formatting Strings

---
+++ FormattingStringsExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormattingStringsExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string director = "Sam Raimi", movie = "Spider man";
+            string formatedString = string.Format("{0} is the director of {1}", director, movie);
+            string formatedString2 = $"{director} is the director of {movie}";
+            Console.WriteLine(formatedString);
+            Console.WriteLine(formatedString2);
+        }
+    }
+}
