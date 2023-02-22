6343f37a0ab59c5a7b7779cbc7487ff8441a4605
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-22 14:50:59 +0530

    248. Octal Number System

---
+++ OctalExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OctalExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dec1 = 289;
+            string oct1 = Convert.ToString(dec1, 8); //decimal to octal
+            Console.WriteLine(oct1);
+
+            int dec2 = Convert.ToInt32(oct1, 8); //octal to decimal
+            Console.WriteLine(dec2);
+
+            Console.ReadKey();
+        }
+    }
+}
