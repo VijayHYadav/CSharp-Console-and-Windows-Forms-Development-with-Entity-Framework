ce6553fdae58fecd8c2fd163255ebdbdeb8dfa47
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-22 07:59:19 +0530

    241. Date Subtraction

---
+++ SubtractDateExample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubtractDateExample
+{
+    class Employee
+    {
+        public string EmployeeName { get; set; }
+        public DateTime DateOfJoining { get; set; }
+        public double ExperienceYears { get; set; }
+        public double ExperienceMonths { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee() { EmployeeName = "John", DateOfJoining = DateTime.Parse("2015-01-01") };
+            //Today: 2021-10-11
+            DateTime today = DateTime.Now;
+            if (today.CompareTo(emp.DateOfJoining) == 1) //1, 0, -1
+            {
+                TimeSpan ts = today.Subtract(emp.DateOfJoining);
+                emp.ExperienceYears = Math.Floor(ts.TotalDays / 365);
+                emp.ExperienceMonths = Math.Floor((ts.TotalDays - (emp.ExperienceYears * 365)) / 30);
+                Console.WriteLine(emp.ExperienceYears + " years and " + emp.ExperienceMonths + " months");
+            }
+            else
+            {
+                Console.WriteLine("Date of joining is greater than today's date. Subtraction is not possible");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
