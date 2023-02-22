92558fc11df7cdc8ea249b4b88a3c2bafddd5fe7
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-22 17:44:39 +0530

    255. 'File' class - Read and Write

---
+++ FileLinesExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileLinesExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //collection
+            List<string> asia = new List<string>() { "Russia", "China", "India" };
+
+            //file path
+            string filePath = @"d:\practice\asia.txt";
+
+            //WriteAllLines
+            File.WriteAllLines(filePath, asia);
+            Console.WriteLine("asia.txt created");
+
+            //ReadAllLines
+            string[] existingContent = File.ReadAllLines(filePath);
+            foreach (string line in existingContent)
+            {
+                Console.WriteLine(line);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FileReadWriteExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileReadWriteExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"d:\practice\dog.txt";
+            string content = "The dog is one of the common domestic animal.";
+
+            //WriteAllText
+            File.WriteAllText(filePath, content);
+            Console.WriteLine("dog.txt created");
+
+            //ReadAllText
+            string s = File.ReadAllText(filePath);
+            Console.WriteLine("\nFile read:");
+            Console.WriteLine(s);
+
+            Console.ReadKey();
+        }
+    }
+}
