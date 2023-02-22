6beaa98e4d1818853e445809bce7eecd41280c4e
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-22 08:11:13 +0530

    242. Date Addition

---
+++ AddingDatesExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddingDatesExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = DateTime.Parse("2022-01-01 12:00 am");
+            DateTime dt_after_10_days = dt.AddDays(10);
+            Console.WriteLine("After 10 days: " + dt_after_10_days);
+            DateTime dt_before_10_days = dt.AddDays(-10);
+            Console.WriteLine("Before 10 days: " + dt_before_10_days);
+
+            DateTime dt_after_20_months_and_5_days = dt.AddMonths(20).AddDays(5);
+            Console.WriteLine("After 20 months and 5 days: " + dt_after_20_months_and_5_days);
+            Console.ReadKey();
+        }
+    }
+}
