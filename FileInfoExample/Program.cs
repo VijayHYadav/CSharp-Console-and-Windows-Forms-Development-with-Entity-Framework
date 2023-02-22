d6500bacad5f6fe228ece26a4b4c6894a004ba10
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-22 18:19:40 +0530

    256. 'FileInfo' class

---
+++ FileInfoExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileInfoExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"d:\practice\japan.txt";
+            string destPath = @"d:\practice\another.txt";
+            string destPath2 = @"d:\practice\somethingelse.txt";
+
+            FileInfo fileInfo = new FileInfo(filePath);
+
+            //Create()
+            fileInfo.Create().Close();
+            Console.WriteLine("japan.txt created");
+
+            //CopyTo()
+            FileInfo fileInfo2 = fileInfo.CopyTo(destPath, true); // true for overide
+            Console.WriteLine(fileInfo2.Name + " created");
+
+            //MoveTo()
+            fileInfo2.MoveTo(destPath2);
+            Console.WriteLine(fileInfo2.Name + " moved");
+
+            //Delete()
+            fileInfo2.Delete();
+            Console.WriteLine(fileInfo2.Name + " deleted");
+            Console.ReadKey();
+        }
+    }
+}
