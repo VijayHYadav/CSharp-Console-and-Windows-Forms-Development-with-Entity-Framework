aab47e0af1ceec2136e5fd277f1d60e25617889e
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-22 08:44:17 +0530

    245. Regular Expressions

---
+++ RegExpExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegExpExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex("^[A-Za-z ]*$"); //for digit only => "^[0-9]*$"
+            Console.WriteLine("Enter a name: ");
+            string inputValue = Console.ReadLine();
+            bool result = regex.IsMatch(inputValue);
+            Console.WriteLine(result);
+            if (result == true)
+            {
+                Console.WriteLine("Valid name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid name");
+            }
+            Console.ReadKey();
+        }
+    }
+}
