8ba067b3199911f775f98578d1b816b6d2d6769b
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-22 17:29:36 +0530

    254. 'File' class

---
+++ FileExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace FileExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"D:\practice\India.txt", filePath2 = @"D:\practice\India2.txt", filePath3 = @"D:\practice\another.txt";
+
+            File.Create(filePath).Close();
+            Console.WriteLine("India.txt created");
+
+            bool exists = File.Exists(filePath);
+            if (exists)
+            {
+                File.Copy(filePath, filePath2);
+                Console.WriteLine("Copied India.txt to India2.txt");
+
+                File.Move(filePath2, filePath3);
+                Console.WriteLine("Moved India2.txt to another.txt");
+
+                File.Delete(filePath3);
+                Console.WriteLine("another.txt deleted");
+            }
+            else
+            {
+                Console.WriteLine("File not found");
+            }
+            Console.ReadKey();
+        }
+    }
+    
+}
