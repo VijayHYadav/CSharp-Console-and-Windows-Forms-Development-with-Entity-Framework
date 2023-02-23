476f9a8a20d1bc72e2cc20b43584b6e9e7a88c34
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-23 08:20:41 +0530

    264. 'FileStream' class - Part 2

--- FileStreamExample/Program.cs
+++ FileStreamExample/Program.cs
@@ -19,8 +19,24 @@ namespace FileStreamExample
             //FileStream fileStream = File.Open(filePath, FileMode.Create, FileAccess.Write);
             //FileStream fileStream = File.OpenWrite(filePath);
             //FileStream fileStream = fileInfo.Create();
-            //FileStream fileStream = fileInfo.Open(FileMode.Create, FileAccess.Write);
-            FileStream fileStream = fileInfo.OpenWrite();
+            FileStream fileStream = fileInfo.Open(FileMode.Create, FileAccess.Write);
+            //FileStream fileStream = fileInfo.OpenWrite();
+
+            //create content
+            string content = "Vijay is one of the domestic animal.";
+            byte[] bytes = System.Text.Encoding.ASCII.GetBytes(content);
+
+            //Write
+            fileStream.Write(bytes, 0, bytes.Length);
+            //some work here
+            string content2 = "other text here";
+            byte[] bytes2 = System.Text.Encoding.ASCII.GetBytes(content2);
+            fileStream.Write(bytes2, 0, bytes2.Length);
+
+            fileStream.Close();
+            Console.WriteLine("dog.txt created");
+
+            Console.ReadKey();
         }
     }
 }
