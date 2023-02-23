05c4cbc7f5bca2734a37648f36a4ad55fff9dc72
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-23 07:55:07 +0530

    263. 'FileStream' class - Part 1

---
+++ FileStreamExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileStreamExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"d:\practice\dog.txt";
+            FileInfo fileInfo = new FileInfo(filePath);
+
+            //FileStream fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write);
+            //FileStream fileStream = File.Create(filePath);
+            //FileStream fileStream = File.Open(filePath, FileMode.Create, FileAccess.Write);
+            //FileStream fileStream = File.OpenWrite(filePath);
+            //FileStream fileStream = fileInfo.Create();
+            //FileStream fileStream = fileInfo.Open(FileMode.Create, FileAccess.Write);
+            FileStream fileStream = fileInfo.OpenWrite();
+        }
+    }
+}
