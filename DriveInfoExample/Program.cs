f49739fca381e5de509425dab04ec1b6fe2da322
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-23 07:37:44 +0530

    262. 'DriveInfo' class

---
+++ DriveInfoExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriveInfoExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DriveInfo driveInfo = new DriveInfo("c:");
+            Console.WriteLine("Name: " + driveInfo.Name);
+            Console.WriteLine("Drive Type: " + driveInfo.DriveType);
+            Console.WriteLine("Volume Label: " + driveInfo.VolumeLabel);
+            Console.WriteLine("Root Directory: " + driveInfo.RootDirectory);
+            Console.WriteLine("Total Size: " + (driveInfo.TotalSize / 1024 / 1024 / 1024) + " gb");
+            Console.WriteLine("Free space: " + (driveInfo.AvailableFreeSpace / 1024 / 1024 / 1024) + " gb");
+            Console.ReadKey();
+
+        }
+    }
+}
