aa38a5c05218ee9f9e93a491680fe82e15a06f16
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-23 07:33:43 +0530

    261. 'DirectoryInfo' class - Properties

---
+++ DirectoryInfoProperties/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryInfoProperties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dirPath = @"c:\practice\world";
+            DirectoryInfo directoryInfo = new DirectoryInfo(dirPath);
+            Console.WriteLine("Exists: " + directoryInfo.Exists);
+            if (directoryInfo.Exists)
+            {
+                Console.WriteLine("Full Name: " + directoryInfo.FullName);
+                Console.WriteLine("Name: " + directoryInfo.Name);
+                Console.WriteLine("Directory Name: " + directoryInfo.Parent);
+                Console.WriteLine("Root: " + directoryInfo.Root);
+                Console.WriteLine("Creation date and time: " + directoryInfo.CreationTime);
+                Console.WriteLine("Last modification date and time: " + directoryInfo.LastWriteTime);
+                Console.WriteLine("Lat access date and time: " + directoryInfo.LastAccessTime);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
