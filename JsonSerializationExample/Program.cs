f4e7c73441368a5bc95880218b55e8839b2a1c20
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-24 17:38:00 +0530

    272. Json Serialization

---
+++ JsonSerializationExample/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace JsonSerializationExample
+{
+    [Serializable]
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public int Age { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create object
+            Customer customer = new Customer() { CustomerId = 1, CustomerName = "Nancy", Age = 20 };
+
+            //create object of JavaScriptSerializer
+            JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
+            string filePath = @"d:\practice\customer.txt";
+            StreamWriter streamWriter = new StreamWriter(filePath);
+
+            //Serialize
+            string json = javaScriptSerializer.Serialize(customer);
+            streamWriter.WriteLine(json);
+            streamWriter.Close();
+            Console.WriteLine(json);
+            Console.WriteLine("Serialized");
+
+            //Deserialize
+            StreamReader streamReader = new StreamReader(filePath);
+            Customer customer_from_file = javaScriptSerializer.Deserialize(streamReader.ReadToEnd(), typeof(Customer)) as Customer;
+            Console.WriteLine(customer_from_file.CustomerId);
+            Console.WriteLine(customer_from_file.CustomerName);
+            Console.WriteLine(customer_from_file.Age);
+            Console.ReadKey();
+        }
+    }
+}
