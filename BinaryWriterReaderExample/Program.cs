8565a75cb533834dfb744bd8bb0d12039522dd2c
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-24 09:55:22 +0530

    268. BinaryWriter and BinaryReader - Part 1

---
+++ BinaryWriterReaderExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryWriterReaderExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            short countryId = 1;
+            string countryName = "France";
+            long population = 65273511;
+            string region = "Western Europe";
+            string filePath = @"d:\practice\france.txt";
+            FileStream fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write);
+            using (BinaryWriter binaryWriter = new BinaryWriter(fileStream))
+            {
+                binaryWriter.Write(countryId); //0001
+                binaryWriter.Write(countryName); //100 0100...
+                binaryWriter.Write(population);
+                binaryWriter.Write(region);
+                //binaryWriter.Close(); //optional (in case of 'using' construct above)
+            }
+            Console.WriteLine("france.txt created");
+
+            Console.ReadKey();
+        }
+    }
+}
