e42c83690128138c945afdc5d22cfaf46ada0b16
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-26 17:53:15 +0530

    294. Target-typed 'new' expressions

--- NullableRefTypes/Program.cs
+++ NullableRefTypes/Program.cs
@@ -1,14 +1,13 @@
 ﻿class Employee
 {
-    //public string FirstName { get; set; } = "default string";
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public AddressDetails? PersonAddressDetails { get; set; }
 
-    public Employee()
+    public Employee(string firstName, string lastName)
     {
-        FirstName = "default string";
-        LastName = "default string";
+        FirstName = firstName;
+        LastName = lastName;
     }
 }
 
@@ -23,7 +22,22 @@ class EmployeeBusinessLogic
     public Employee? GetEmployee()
     {
         //return null;
-        return new Employee();
+        //return new Employee();
+        //return new("John", "Doe"); //equivalent to new Employee("John", "Doe")
+        //Employee e = new("John", "Doe");
+        Employee e = new("John", "Doe");
+        return e;
+    }
+}
+
+class DataPrinter
+{
+    public static void PrintEmployee(Employee e)
+    {
+        if (e != null)
+        {
+            Console.WriteLine($"{e.FirstName} {e.LastName}");
+        }
     }
 }
 class Program
@@ -33,17 +47,26 @@ class Program
         EmployeeBusinessLogic employeeBusinessLogic = new EmployeeBusinessLogic();
         Employee? employee = employeeBusinessLogic.GetEmployee();
 
+        //we are checking if it is null before the "if" statement
         bool isValid = employee == null;
         if (isValid)
         {
             Console.WriteLine("The variable is null");
         }
-        else
+        else //compiler can't understand above condition that checks if the variable is null
         {
-            Console.WriteLine(employee!.FirstName.ToUpper());
+            Console.WriteLine(employee!.FirstName.ToUpper()); //!. operator (null forgiving operator) says that, the "employee" variable is NOT NULL, I'm sure.
             Console.WriteLine(employee!.LastName.ToUpper());
-            Console.WriteLine(employee!.PersonAddressDetails?.City); // ?. null propagation
-            Console.WriteLine(employee!.PersonAddressDetails?.ZipCode); // ?. null propagation
+            Console.WriteLine(employee!.PersonAddressDetails?.City);
+            Console.WriteLine(employee!.PersonAddressDetails?.ZipCode); //?. operator (null propagation operator) says that, access that "City" or "ZipCode" property ONLY if  "employee" variable is NOT NULL; otherwise, make the whole expression as null.
+        }
+
+        DataPrinter.PrintEmployee(new("one", "two")); //Employee class name will be taken automatically here, based on the definition of the PrintEmployee method
+
+        //"new" expression works here, because we have not used "var"
+        using (FileStream f = new("", FileMode.Create, FileAccess.Read))
+        {
+
         }
         Console.ReadKey();
     }
