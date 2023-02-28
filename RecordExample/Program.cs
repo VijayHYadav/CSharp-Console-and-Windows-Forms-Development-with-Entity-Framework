f2fe5bf84aab981a863e1027a1216aafba664239
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-28 10:49:46 +0530

    312. Records - Immutability

--- RecordExample/Program.cs
+++ RecordExample/Program.cs
@@ -1,15 +1,19 @@
 ﻿namespace records_example
 {
     public record Person(string Name, int Age, Address PersonAddress);
-    public record Address(string City);
+    public record Address(string City)
+    {
+        public string? Country { get; set; }
+    }
+
     class Program
     {
         static void Main()
         {
             Person person1 = new Person("John", 20, new Address("London"));
-            Person person2 = new("Scott", 22, new Address("New Delhi"));
-            Console.WriteLine($"{person1.Name}, {person1.Age}, {person1.PersonAddress.City}");
-            Console.WriteLine($"{person2.Name}, {person2.Age}, {person2.PersonAddress.City}");
+            person1.PersonAddress.Country = "UK";
+
+            Console.WriteLine($"{person1.Name}, {person1.Age}, {person1.PersonAddress.City}, {person1.PersonAddress.Country}");
             //person1.Name = "changed"; //Error: Init-only property can only be assigned in a constructor or object initializer
             Console.ReadKey();
         }
