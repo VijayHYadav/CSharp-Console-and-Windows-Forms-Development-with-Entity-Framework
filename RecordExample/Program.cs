06d1f45dae6ee75a48dfa02a59a38246c6917dc3
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-28 10:43:50 +0530

    311. Records - Nested Records

--- RecordExample/Program.cs
+++ RecordExample/Program.cs
@@ -1,14 +1,17 @@
-﻿public record Person(string Name, int Age);
-
-class Program
+﻿namespace records_example
 {
-    static void Main()
+    public record Person(string Name, int Age, Address PersonAddress);
+    public record Address(string City);
+    class Program
     {
-        Person person1 = new Person("John", 20);
-        Person person2 = new("Scott", 22);
-        Console.WriteLine($"{person1.Name}, {person1.Age}");
-        Console.WriteLine($"{person2.Name}, {person2.Age}");
-        //person1.Name = "changed"; //Error: Init-only property can only be assigned in a constructor or object initializer
-        Console.ReadKey();
+        static void Main()
+        {
+            Person person1 = new Person("John", 20, new Address("London"));
+            Person person2 = new("Scott", 22, new Address("New Delhi"));
+            Console.WriteLine($"{person1.Name}, {person1.Age}, {person1.PersonAddress.City}");
+            Console.WriteLine($"{person2.Name}, {person2.Age}, {person2.PersonAddress.City}");
+            //person1.Name = "changed"; //Error: Init-only property can only be assigned in a constructor or object initializer
+            Console.ReadKey();
+        }
     }
 }
