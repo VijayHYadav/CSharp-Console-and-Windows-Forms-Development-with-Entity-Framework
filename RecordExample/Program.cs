5242c8e92f2a65c76405482ef209b0f7bd22bc3c
VijayHYadav <vizzyadav1997@gmail.com>
2023-02-28 15:31:01 +0530

    314. Records - 'with' Expression

--- RecordExample/Program.cs
+++ RecordExample/Program.cs
@@ -1,23 +1,23 @@
 ﻿namespace records_example
 {
-    public record Person(string Name, int Age, Address PersonAddress);
+    public record Person(string Name, Address PersonAddress)
+    {
+        public int Age { get; set; }
+    }
     public record Address(string City, string Country);
 
     class Program
     {
         static void Main()
         {
-            Person person1 = new Person("John", 20, new Address("London", "UK"));
-            Person person2 = new Person("John", 20, person1.PersonAddress);
+            Person person1 = new Person("John", new Address("London", "UK"));
+            person1.Age = 20;
+            //Person person2 = person1; //reference copy
+            Person person2 = person1 with { Age = 50, Name = "Scott" }; //shallow copy
+            person1.Age = 30;
 
-            Console.WriteLine($"{person1.Name}, {person1.Age}, {person1.PersonAddress.City}, {person1.PersonAddress.Country}");
-            Console.WriteLine($"{person2.Name}, {person2.Age}, {person2.PersonAddress.City}, {person2.PersonAddress.Country}");
-            //person1.Name = "changed"; //Error: Init-only property can only be assigned in a constructor or object initializer
-
-            Console.Write(person1.Equals(person2)); //True
-            Console.Write(person1 == person2); //True
-            Console.Write(person1 != person2); //False
-            //Console.Write(person1 != person1.PersonAddress); //Error
+            Console.WriteLine($"{person1.Name}, {person1.Age}, {person1.PersonAddress.City}, {person1.PersonAddress.Country}"); //John, 30, London, UK
+            Console.WriteLine($"{person2.Name}, {person2.Age}, {person2.PersonAddress.City}, {person2.PersonAddress.Country}"); //Scott, 50, London, UK
             Console.ReadKey();
         }
     }
